3331e14a25d096b3920b9698043c1aa3640d0b83
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-05-28 11:54:14 -0500

    Adding notes

--- Program.cs
+++ Program.cs
@@ -9,16 +9,32 @@ namespace lists
         {
             List<string> planetList = new List<string>(){"Mercury", "Mars"};
 
+            // 1. Add() Jupiter and Saturn at the end of the list.
+
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
 
             // planetList.ForEach(planet => Console.WriteLine(planet));
 
+            // 2.Create another List that contains that last two planet of our solar system.
+
             List<string> morePlanets = new List<string>(){"Uranus", "Neptune"};
+
+            // 3. Combine the two lists by using AddRange()
 
             planetList.AddRange(morePlanets);
 
-            planetList.ForEach(planet => Console.WriteLine(planet));
+            // planetList.ForEach(planet => Console.WriteLine(planet));
+
+            // 4. Use Insert() to add Earth, and Venus in the correct order.
+
+            
+
+            // 5. Use Add() again to add Pluto to the end of the list.
+            // 6. Now that all the planets are in the list, slice the list using GetRange() in order to extract the rocky planets into a new list called rockyPlanets. The rocky planets will remain in the original planets list.
+            // 7. Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method to eliminate it from the end of planetList.
+
+
         }
     }
 }
